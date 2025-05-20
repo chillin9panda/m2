da299cfee71a43f5cb7e10bd3c3751f2bcda0962
chillin9panda <duremarkastu@gmail.com>
2025-05-20 18:43:31 +0300

    api test works at localhost:5173/test

---
+++ frontend/src/config.ts
@@ -0,0 +1,6 @@
+// For development
+const BASE_URL = window.location.hostname === "localhost:8000";
+// For production
+// const BASE_URL = 'https://your-production-api.com';
+
+export const API_BASE_URL = BASE_URL;
